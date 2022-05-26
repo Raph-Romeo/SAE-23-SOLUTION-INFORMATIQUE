7c27ef4c6580e7a366334595868e0a5cd77f9af6
Raph-Romeo <95073137+Raph-Romeo@users.noreply.github.com>
2022-05-26 15:04:40 +0200

    First
    Les formulaires et les modèles django sont configurés.

---
+++ SAE23/adminserver/forms.py
@@ -0,0 +1,58 @@
+from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
+from . import models
+
+class type_de_serveursForm(ModelForm):
+    class Meta:
+        model = models.type_de_serveurs
+        fields = ('type', 'description')
+        labels = {
+            'type' : _('Type de serveur:'),
+            'description' : _('Description:') ,
+        }
+
+class serveursForm(ModelForm):
+    class Meta:
+        model = models.serveurs
+        fields = ('nom','type_de_serveur','processeurs','memoire','stockage')
+        labels = {
+            'nom' : _('Nom du serveur:'),
+            'type_de_serveur' : _('Type du serveur:'),
+            'processeurs' : _('Nombre de processeurs:'),
+            'memoire' : _('Memoire du serveur (MegaOctets):'),
+            'stockage' : _('Espace de stockage du serveur (GigaOctets):'),
+        }
+
+class utilisateursForm(ModelForm):
+    class Meta:
+        model = models.utilisateurs
+        fields = ('nom','prenom','email')
+        labels = {
+            'nom' : _("Nom de l'utilisateur:"),
+            'prenom' : _("Prenom de l'utilisateur:"),
+            'email' : _("Email de l'utilisateur:"),
+        }
+
+class servicesForm(ModelForm):
+    class Meta:
+        model = models.services
+        fields = ('nom_service','date_de_lancement','espace_memoire_utilise','memoire_vive_necessaire','serveur_de_lancement')
+        labels = {
+            'nom_service' : _('Nom du service:'),
+            'date_de_lancement' : _('Date de lancement du service:'),
+            'espace_memoire_utilise' : _('Espace memoire utilisee:'),
+            'memoire_vive_necessaire' : _('Memoire vive necessaire:'),
+            'serveur_de_lancement' : _('Serveur de lancement:'),
+        }
+        localized_fields = ('date_de_lancement',)
+
+class applications(ModelForm):
+    class Meta:
+        model = models.services
+        fields = ('nom_application','logo','serveurs','utilisateurs')
+        labels = {
+            'nom_application' : _("Nom de l'application:"),
+            'logo' : _('Logo:'),
+            'serveurs' : _('Serveurs:'),
+            'utilisateurs' : _("Utilisateurs de l'application:"),
+        }
---
+++ SAE23/adminserver/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+# Create your models here.
+
+class type_de_serveurs(models.Model):
+    type = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.type
+
+    def dico(self):
+        return {"type": self.type,"description":self.description}
+
+class serveurs(models.Model):
+    nom = models.CharField(max_length=100,blank=False)
+    type_de_serveur = models.ForeignKey(type_de_serveurs, on_delete=models.CASCADE, null="true")
+    processeurs = models.IntegerField(blank=False)
+    memoire = models.IntegerField(blank=False)
+    stockage = models.IntegerField(blank=False)
+
+
+    def __str__(self):
+        return self.nom
+
+    def dico(self):
+        return {"nom": self.nom,"type_de_serveur":self.type_de_serveur,"processeurs":self.processeurs,"memoire":self.memoire,"stockage":self.stockage}
+
+class utilisateurs(models.Model):
+    nom = models.CharField(max_length=100,blank=False)
+    prenom = models.CharField(max_length=100,blank=False)
+    email = models.EmailField(blank=False, null=True)
+
+    def __str__(self):
+        return self.nom + " " + self.prenom
+
+    def dico(self):
+        return {"nom": self.nom,"prenom":self.prenom,"email":self.email}
+
+
+class services(models.Model):
+    nom_service = models.CharField(max_length=100,blank=False)
+    date_de_lancement = models.DateField(max_length=100,blank=False)
+    espace_memoire_utilise = models.IntegerField(blank=False)
+    memoire_vive_necessaire = models.IntegerField(blank=False)
+    serveur_de_lancement = models.ForeignKey(serveurs, on_delete=models.CASCADE, null="true")
+
+    def __str__(self):
+        return self.nom_service
+
+    def dico(self):
+        return {"nom_service":self.nom_service,"date_de_lancement":self.date_de_lancement,"espace_memoire_utilise":self.espace_memoire_utilise,"memoire_vive_necessaire":self.memoire_vive_necessaire,"serveur_de_lancement":self.serveur_de_lancement}
+
+class applications(models.Model):
+    nom_application = models.CharField(max_length=100,blank=False)
+    logo = models.ImageField(upload_to='images')
+    serveurs = models.ForeignKey(serveurs, on_delete=models.CASCADE, null="true")
+    utilisateurs = models.ForeignKey(utilisateurs, on_delete=models.CASCADE, null="true")
+
+    def __str__(self):
+        return self.nom_application
+
+    def dico(self):
+        return {"nom_application":self.nom_application,"logo":self.logo,"serveurs":self.serveurs,"utilisateurs":self.utilisateurs}
